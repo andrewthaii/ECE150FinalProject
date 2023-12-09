ced404b5be8a67eb1a7d76aa2a1d4071a709392a
bonniehuynh <bonniehuynh@ucsb.edu>
2023-12-09 15:35:20 -0800

    added firebase login and register

--- app/src/main/java/com/example/habittracker/MainActivity.java
+++ app/src/main/java/com/example/habittracker/MainActivity.java
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -43,10 +44,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements CustomBottomSheetDialogFragment.OnBottomSheetDismissListener {
-
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
     private ArrayList<String> habitArray;
     private ArrayAdapter adapter;
     private ListView habitList;
@@ -61,6 +66,30 @@ public class MainActivity extends AppCompatActivity implements CustomBottomSheet
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // for firebase login and register
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(user.getEmail());
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         // i dont think this does anything I just added it from hw 2
         Toolbar toolbar = findViewById(R.id.toolbar);
